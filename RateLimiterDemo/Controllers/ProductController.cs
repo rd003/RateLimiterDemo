7f6d7c36055f9db73f5b61c0256e048f8338630f
Ravindra Devrani <ravindra.devrani003@gmail.com>
2023-05-23 18:51:28 +0530

    Add project files.

---
+++ RateLimiterDemo/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.RateLimiting;
+
+namespace RateLimiterDemo.Controllers
+{
+    [EnableRateLimiting("fixed")]
+    [ApiController]
+    [Route("api/products")]
+    public class ProductController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("data fetched");
+        }
+
+        [HttpGet("/api/products/test")]
+        public IActionResult test()
+        {
+            return Ok("test....");
+        }
+    }
+}
---
+++ RateLimiterDemo/Program.cs
@@ -0,0 +1,55 @@
+using Microsoft.AspNetCore.RateLimiting;
+using System.Threading.RateLimiting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddRateLimiter(options =>
+{
+    // options.RejectionStatusCode = 429;
+    options.OnRejected = async (context, token) =>
+    {
+        context.HttpContext.Response.StatusCode = 429;
+        if (context.Lease.TryGetMetadata(MetadataName.RetryAfter, out var retryAfter))
+        {
+            await context.HttpContext.Response.WriteAsync($"Too many requests. Please try again after {retryAfter.TotalSeconds} second(s)", cancellationToken: token);
+        }
+        else
+        {
+            await context.HttpContext.Response.WriteAsync("Too many requests. Please try again after some time");
+        }
+    };
+    options.AddFixedWindowLimiter(policyName: "fixed", options =>
+    {
+        options.PermitLimit = 4;
+        options.Window = TimeSpan.FromSeconds(12);
+        options.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
+        options.QueueLimit = 2;
+    });
+}
+    );
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseRateLimiter();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
